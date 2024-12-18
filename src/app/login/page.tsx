2c6a2c92c85427969bc80b4b0e1c76df3669aa8b
gabeFrancisco <gabegit2@gmail.com>
2024-12-17 23:03:39 -0300

    criado pagina de login de teste | primeiro teste

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+'use client'
+
+import React, { useState } from 'react'
+import { signIn } from "next-auth/react";
 
 function page() {
+  // const session = useSession();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = () => {
+    signIn("credentials", {
+      email: email,
+      password: password,
+      redirect: false,
+    });
+  }
   return (
     <div>
-      <form action=""></form>
+      <form action="">
+        <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" name='password' value={password} onChange={e => setPassword(e.target.value)}/>
+        <button type='button' onClick={handleSubmit}>Login</button>
+      </form>
     </div>
   )
 }
